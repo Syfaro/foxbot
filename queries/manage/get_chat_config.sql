696488662d0014d85e77ee6aa0d94c7295bd1bf5
Syfaro <syfaro@huefox.com>
2024-02-09 21:14:22 -0500

    Weird potential query performance fix.

--- queries/manage/get_chat_config.sql
+++ queries/manage/get_chat_config.sql
@@ -1,10 +1,13 @@
+WITH lookup_result AS (
+    SELECT
+        lookup_chat($1, $2) chat_id
+)
 SELECT
     DISTINCT ON (name) name,
     value
 FROM
-    group_config
-WHERE
-    chat_id = lookup_chat($1, $2)
+    lookup_result
+    JOIN group_config ON group_config.chat_id = lookup_result.chat_id
 ORDER BY
     name,
     updated_at DESC;
